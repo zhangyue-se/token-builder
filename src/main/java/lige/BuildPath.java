fb8a9afcb1e9ab6fe12938cb096125e69f890243
Zhang Yue <976445200@qq.com>
2021-04-25 20:09:22 +0800

    层次路径的构建

--- src/main/java/lige/BuildPath.java
+++ src/main/java/lige/BuildPath.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,7 +25,12 @@ public class BuildPath {
         List<List<Token>> sequenceList = getSequenceList(path);
         sequenceList.forEach(temp->{
             String preorderPath = getPreorderPath(temp);
+            // TODO: 2021/4/25 将前序序列和层次序列对应起来，条数和预测节点
             System.out.println(preorderPath);
+            List<String> hierarchicalPath = getHierarchicalPath(temp);
+            //将序列按照长度排序，短到长
+            Comparator<String> comparatorByLength = Comparator.comparingInt(String::length);
+            hierarchicalPath.stream().sorted(comparatorByLength).forEach(System.out::println);
         });
     }
 
@@ -87,11 +93,12 @@ public class BuildPath {
 
     /**
      * 获得层次结构的路径
-     * @param sequence
-     * @return
      */
-    public String getHierarchicalPath(List<Token> sequence){
-        return null;
+    public static List<String> getHierarchicalPath(List<Token> sequence){
+        List<String> temp = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        dfs2(sequence, temp, result, 0);
+        return result;
     }
 
 
@@ -101,7 +108,7 @@ public class BuildPath {
     public static String getPreorderPath(List<Token> sequence){
         List<String> result = new ArrayList<>();
         //第一个token从0开始
-        dfs(sequence,result,0);
+        dfs1(sequence,result,0);
         StringBuilder stringBuilder = new StringBuilder();
         for (String ss : result){
             stringBuilder.append(ss);
@@ -114,12 +121,11 @@ public class BuildPath {
      * dfs实现先序遍历
      * @param child 当前token的孩子坐标
      */
-    public static void dfs(List<Token> sequence,List<String> result,Integer child){
-
+    public static void dfs1(List<Token> sequence,List<String> result,Integer child){
         if (!sequence.get(child).isChildrenEmpty()){
             result.add(sequence.get(child).getType());
             for (Integer integer:sequence.get(child).getChildren()){
-                dfs(sequence,result,integer);
+                dfs1(sequence,result,integer);
             }
         }else {
             for (String name:sequence.get(child).getValue()){
@@ -129,5 +135,24 @@ public class BuildPath {
         }
     }
 
+    /**
+     * dfs实现父节点序列的获取
+     * @param temp  临时存放当前的父节点
+     * @param result    最终的序列，每个有type和value节点的序列
+     * @param child 孩子节点的序号
+     */
+    public static void dfs2(List<Token> sequence,List<String> temp, List<String> result, Integer child){
+        if (sequence.get(child).isChildrenEmpty()){
+            StringBuilder stringBuilder = new StringBuilder();
+            temp.forEach(token->stringBuilder.append(token + ","));
+            result.add(stringBuilder.substring(0,stringBuilder.length()-1));
+        }else {
+            temp.add(sequence.get(child).getType());
+            sequence.get(child).getChildren().forEach(childPos->{
+                List<String> temp1 = new ArrayList<>(temp);
+                dfs2(sequence, temp1, result, childPos);
+            });
+        }
+    }
 
 }
